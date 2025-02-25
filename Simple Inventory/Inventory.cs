71836a9fc620da0de0dec580a5440ac10f743b99
ayabaara <ayabaara4@gmail.com>
2025-02-25 11:22:18 +0200

    edit products in inventory class to private field & allow read only outside  the class

--- Simple Inventory/Inventory.cs
+++ Simple Inventory/Inventory.cs
@@ -3,69 +3,71 @@ namespace SimpleInventory
 {
        class Inventory
        {
-        public Dictionary<int, Product> Products { get; }
-        public Inventory()
-        {
-            Products = new Dictionary<int, Product>();
-        }
+            private Dictionary<int, Product> products = new Dictionary<int, Product>();
+        public IReadOnlyDictionary<int, Product> Products => products;
 
-        public bool AddProduct(Product product)
-        {
-            if (Products.ContainsKey(product.ID))
+        public Inventory()
             {
-                Console.WriteLine($"Product '{product.ID}' already exists. Not added.");
+                products = new Dictionary<int, Product>();
+            }
+
+            public bool AddProduct(Product product)
+            {
+                if (products.ContainsKey(product.ID))
+                {
+                    Console.WriteLine($"Product '{product.ID}' already exists. Not added.");
+                    return false;
+                }
+                else
+                {
+                    products.Add(product.ID, product);
+                    return true;
+                }
+            }
+
+
+            public Product searchProduct(int ID)
+            {
+                if (products.ContainsKey(ID))
+                {
+                    return products[ID]; 
+                }
+                return null; 
+            }
+
+            public bool editProduct(Product modifiedProduct,int ID)
+            {
+               Product product = searchProduct(ID);
+                if (product != null)
+                {
+                    if (modifiedProduct.Name != null)
+                    {
+                        product.Name = modifiedProduct.Name;
+                    }
+                    if (modifiedProduct.Price != -1)
+                    {
+                        product.Price = modifiedProduct.Price;
+                    }
+                    if (modifiedProduct.Quantity != -1)
+                    {
+                        product.Quantity = modifiedProduct.Quantity;
+                    }
+                    return true;
+
+                }
+                return false;
+
+
+            }
+
+            public bool deleteProduct (int ID)
+            {
+                if (products.ContainsKey(ID)) {
+                products.Remove(ID);
+                    return true;
+                }
                 return false;
             }
-            else
-            {
-                Products.Add(product.ID, product);
-                return true;
-            }
         }
-
-
-        public Product searchProduct(int ID)
-        {
-            if (Products.ContainsKey(ID))
-            {
-                return Products[ID]; 
-            }
-            return null; 
-        }
-
-        public bool editProduct(Product modifiedProduct,int ID)
-        {
-           Product product = searchProduct(ID);
-            if (product != null)
-            {
-                if (modifiedProduct.Name != null)
-                {
-                    product.Name = modifiedProduct.Name;
-                }
-                if (modifiedProduct.Price != -1)
-                {
-                    product.Price = modifiedProduct.Price;
-                }
-                if (modifiedProduct.Quantity != -1)
-                {
-                    product.Quantity = modifiedProduct.Quantity;
-                }
-                return true;
-
-            }
-            return false;
-
-
-        }
-
-        public bool deleteProduct (int ID)
-        {
-            if (Products.ContainsKey(ID)) {
-                Products.Remove(ID);
-                return true;
-            }
-            return false;
-        }
-    }
 
 }
