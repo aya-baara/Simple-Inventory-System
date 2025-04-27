8956094dcdbb3e92b3dd7e0e66a007d47d258fbb
ayabaara <ayabaara4@gmail.com>
2025-04-27 15:43:28 +0300

    extract open function

--- Simple Inventory/DataBaseConnection/MsSQLConnection.cs
+++ Simple Inventory/DataBaseConnection/MsSQLConnection.cs
@@ -6,11 +6,5 @@ class MsSQLConnection
 {
     public static string MsSqlConnectionString = "Server=DESKTOP-NKVRDQD;Database=SimpleInventory;Trusted_Connection=True;TrustServerCertificate=True;";
 
-    public static SqlConnection GetOpenConnection()
-    {
-        var conn = new SqlConnection(MsSQLConnection.MsSqlConnectionString);
-        conn.Open();
-        return conn;
-    }
 }
 
