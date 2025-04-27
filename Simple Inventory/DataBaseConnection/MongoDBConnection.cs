97c3095fb17cff19bd34fd8675e6996270d83773
ayabaara <ayabaara4@gmail.com>
2025-04-27 17:37:26 +0300

    add Mongo connection

---
+++ Simple Inventory/DataBaseConnection/MongoDBConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple_Inventory.DataBaseConnection;
+public static class MongoDBConnection
+{
+    public static string ConnectionString = "mongodb://localhost:27017"; 
+    public static string DatabaseName = "SimpleInventoryDB";
+    public static string CollectionName = "Products";
+}
+
