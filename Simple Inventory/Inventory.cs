1cfc1fd0306b7ae5e582c6fb0eb2f04282d37533
ayabaara <ayabaara4@gmail.com>
2025-04-27 16:46:13 +0300

    addIDataBase for dependency injection and update methods to use it

--- Simple Inventory/Inventory.cs
+++ Simple Inventory/Inventory.cs
@@ -1,84 +1,38 @@
-﻿
-using Simple_Inventory.DataBaseConnection;
-using Microsoft.Data.SqlClient;
+﻿using Simple_Inventory.Interfaces;
 
-namespace SimpleInventory
+namespace SimpleInventory;
+
+class Inventory
 {
-       class Inventory
-       {
-            private Dictionary<int, Product> products = new Dictionary<int, Product>();
-            public IReadOnlyDictionary<int, Product> Products => products;
+    private IDataBase dataBase;
 
-            public Inventory()
-            {
-                products = new Dictionary<int, Product>();
-            }
+    public Inventory(IDataBase dataBase)
+    {
+        dataBase = dataBase;
+    }
 
-            public bool AddProduct(Product product)
-            {
-                try
-                {
-                    using (var conn = MsSQLConnection.GetOpenConnection())
-                    {
-                        conn.Open();
-                        string sql = "INSERT INTO Products (Product_id,Name, Price, Quantity) VALUES (@Id,@Name, @Price, @Quantity)";
+    public bool AddProduct(Product product)
+    {
+        if (dataBase.CheckIfExist(product.ID)) return false;
+        return dataBase.AddProduct(product);
+    }
 
-                        using (SqlCommand cmd = new SqlCommand(sql, conn))
-                        {
-                            cmd.Parameters.AddWithValue("@Id", product.ID);
-                            cmd.Parameters.AddWithValue("@Name", product.Name);
-                            cmd.Parameters.AddWithValue("@Price", product.Price);
-                            cmd.Parameters.AddWithValue("@Quantity", product.Quantity);
+    public Product? SearchProduct(int id)
+    {
+        return dataBase.Search(id);
+    }
 
-                            int rowsAffected = cmd.ExecuteNonQuery();
+    public bool EditProduct(Product modifiedProduct)
+    {
 
-                            return rowsAffected > 0; 
-                        }
-                    }
-                }
-                catch
-                {
-                    return false; 
-                }
-            }
+        return dataBase.UpdateProduct(modifiedProduct);
 
+    }
 
-
-            public Product? SearchProduct(int id)
-                {
-                    return products.GetValueOrDefault(id, null);
-                }
-
-            public bool EditProduct(Product modifiedProduct,int id)
-            {
-               Product product = SearchProduct(id);
-                if (product != null)
-                {
-                    if (modifiedProduct.Name != null)
-                    {
-                        product.Name = modifiedProduct.Name;
-                    }
-                    if (modifiedProduct.Price != -1)
-                    {
-                        product.Price = modifiedProduct.Price;
-                    }
-                    if (modifiedProduct.Quantity != -1)
-                    {
-                        product.Quantity = modifiedProduct.Quantity;
-                    }
-                    return true;
-
-                }
-                return false;
-
-
-            }
-
-            public bool DeleteProduct (int id)
-            {
-                
-                return products.Remove(id); ;
-            }
-        }
-
+    public bool DeleteProduct(int id)
+    {
+        return dataBase.Delete(id);
+    }
 }
+
+
