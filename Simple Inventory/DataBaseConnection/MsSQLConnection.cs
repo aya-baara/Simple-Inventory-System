e7005a40dd3aad2e6e181f342277a0d7e6841700
ayabaara <ayabaara4@gmail.com>
2025-04-27 11:25:28 +0300

    add sql connection

---
+++ Simple Inventory/DataBaseConnection/MsSQLConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple_Inventory.DataBaseConnection;
+
+class MsSQLConnection
+{
+    public static string MsSqlConnectionString = "Server=DESKTOP-NKVRDQD;Database=SimpleInventory;Trusted_Connection=True;TrustServerCertificate=True;";
+
+}
+
