6df35f28e308eae32b450d470479c085f10e517d
ayabaara <ayabaara4@gmail.com>
2025-04-27 11:31:37 +0300

    add helper method to open the connection

--- Simple Inventory/DataBaseConnection/MsSQLConnection.cs
+++ Simple Inventory/DataBaseConnection/MsSQLConnection.cs
@@ -1,8 +1,16 @@
-﻿namespace Simple_Inventory.DataBaseConnection;
+﻿using Microsoft.Data.SqlClient;
+
+namespace Simple_Inventory.DataBaseConnection;
 
 class MsSQLConnection
 {
     public static string MsSqlConnectionString = "Server=DESKTOP-NKVRDQD;Database=SimpleInventory;Trusted_Connection=True;TrustServerCertificate=True;";
 
+    public static SqlConnection GetOpenConnection()
+    {
+        var conn = new SqlConnection(MsSQLConnection.MsSqlConnectionString);
+        conn.Open();
+        return conn;
+    }
 }
 
