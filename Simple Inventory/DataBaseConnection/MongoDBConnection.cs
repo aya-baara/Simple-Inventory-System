9a6c44725e6841be936b834c6bf0f22c0768f0b1
ayabaara <ayabaara4@gmail.com>
2025-04-29 19:23:29 +0300

    use ConfigurationBuilder to load connection strings from JSON file

--- Simple Inventory/DataBaseConnection/MongoDBConnection.cs
+++ Simple Inventory/DataBaseConnection/MongoDBConnection.cs
@@ -1,8 +1,21 @@
-﻿namespace Simple_Inventory.DataBaseConnection;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Simple_Inventory.DataBaseConnection;
 public static class MongoDBConnection
 {
-    public static string ConnectionString = "mongodb://localhost:27017"; 
-    public static string DatabaseName = "SimpleInventoryDB";
-    public static string CollectionName = "Products";
+    public static string ConnectionString { get; private set; }
+    public static string DatabaseName { get; private set; }
+    public static string CollectionName { get; private set; }
+
+    public static void LoadConnection()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("ConnectionName.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        ConnectionString = configuration["Mongo:ConnectionString"];
+        DatabaseName = configuration["Mongo:DatabaseName"];
+        CollectionName = configuration["Mongo:CollectionName"];
+    }
 }
 
--- Simple Inventory/DataBaseConnection/MsSQLConnection.cs
+++ Simple Inventory/DataBaseConnection/MsSQLConnection.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Simple_Inventory.DataBaseConnection;
 
 class MsSQLConnection
 {
-    public static string MsSqlConnectionString = "Server=DESKTOP-NKVRDQD;Database=SimpleInventory;Trusted_Connection=True;TrustServerCertificate=True;";
+    public static string MsSqlConnectionString { get; private set; }
+
+    public static void LoadConnection()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("ConnectionName.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        MsSqlConnectionString = configuration["SQL:MsSqlConnectionString"];
+    }
+
 
 }
 
